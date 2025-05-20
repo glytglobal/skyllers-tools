f3360866a1a0a22792ae4b0878a989287f75bd51
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-05-20 17:08:58 -0300

    `[ Takanashi - Tools  ]`

---
+++ index.js
@@ -0,0 +1,14 @@
+const { chatgpt, mylogic, takanashi, github, vault, yotools } = require("./soft");
+
+module.exports = {
+  ai: {
+    chatgpt,
+    mylogic,
+    takanashi
+  },
+  uploader: {
+    github,
+    vault,
+    yotools
+  },
+};
